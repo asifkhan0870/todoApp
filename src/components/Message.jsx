088c7928f56bc167047a38a90839adb4e2b95d1e
Asif Khan <khanasif0870@gmail.com>
2024-02-15 01:19:40 +0530

    little bit change in message.jsx

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -3,7 +3,7 @@ import css from './Message.module.css';
 
 const Message=()=>{
       
-    return <p className={css.welcomePara}>There is no work at this time !</p>
+    return <p className={css.welcomePara}>You have no task  at this time Enjoy your day !</p>
 };
 
 export default Message;
